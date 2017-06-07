24ad238c1af7b70ecbd3479b44e592d2a9dc3cdd
erinlevine <erinclevine@gmail.com>
2017-06-07 18:20:27 -0400

    The database remembers things

--- logic.js
+++ logic.js
@@ -7,3 +7,34 @@ var config = {
     messagingSenderId: "539883479478"
   };
   firebase.initializeApp(config);
+
+// Create a variable to reference the database
+var database = firebase.database();
+
+//When the submit button is clicked, we will run the snapshot function below.
+$("#click-button").on("click", function() {
+      // Prevent the page from refreshing
+      event.preventDefault();
+
+      // Grabs user input
+	  var trainNameForm = $("#trainNameForm").val().trim();
+		console.log(trainNameForm);
+	  var destinationForm = $("#desinationForm").val().trim();
+		console.log(destinationForm);
+//	  var firstTrainTimeForm = moment($("#trainTimeForm").val().trim().format('HHmm'));
+//	  var frequencyForm = $("#frequencyForm").val().trim().format("mm");
+
+	  // Creates local "temporary" object for holding inputs
+	  var newTrain = {
+		train: trainNameForm,
+		destination: destinationForm,
+//		first: firstTrainTimeForm,
+//		frequency: frequencyForm
+    };
+	//Setting the new values in the database
+	database.ref().push(newTrain);
+	
+	//Console.logging to make sure the new data has been stored to the database
+	console.log(newTrain.train);
+  	console.log(newTrain.destination);
+	});
